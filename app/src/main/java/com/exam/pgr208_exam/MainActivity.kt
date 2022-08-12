ef611f3c38316ae575e1a2f29a0db93a3232d104
Bahast <bahastagha@gmail.com>
2022-08-13 01:29:11 +0200

    test

---
+++ app/src/main/java/com/exam/pgr208_exam/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.exam.pgr208_exam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private val x = ""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
